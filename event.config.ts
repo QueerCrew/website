ae7f0f8978f0542542ada6cbe1b81d50f9a6d1a0
Alyx Freuwört <33777960+freuwoert@users.noreply.github.com>
2025-04-05 16:42:53 +0200

    Add generic special events

--- event.config.ts
+++ event.config.ts
@@ -13,15 +13,31 @@ export default [
         timezone: 'Europe/Berlin',
         start: '2025-01-01 18:00',
         end: '2075-01-01 22:00',
-        title: 'QC_IRL – Hangout & Chill',
-        description: 'Entspannte Spiele, nette Gespräche und ein paar Drinks. Komm zu unseren qc_irl Treffen jeden 2. und 4. Dienstag im Monat!<br><br>Wir freuen uns auf euch!<br>Euer QC Team ❤️',
+        title: 'QC_IRL – Special Event',
+        description: 'Komm zu unseren qc_irl Treffen jeden 2. und 4. Dienstag im Monat!<br><br>Wir freuen uns auf euch!<br>Euer QC Team ❤️',
         location: 'Onkel Emma, Echternstraße 9',
-        image: '/images/events/qc_irl_hangout_and_chill.png',
+        image: '/images/events/general_special_event_2025.png',
         icsFile: '/downloads/qc-irl-events.ics',
         repeat: {
             generateAmount: 4,
             // @ts-ignore
-            generateFunction: () => dayjs().add(-1, 'day').recur().every('Tuesday').daysOfWeek().every([1,3]).except('2025-04-08').weeksOfMonthByDay(),
+            generateFunction: () => dayjs().add(-1, 'day').recur().every('Tuesday').daysOfWeek().every([1]).except('2025-04-08').weeksOfMonthByDay(),
+        },
+    },
+    {
+        category: 'qc_irl',
+        timezone: 'Europe/Berlin',
+        start: '2025-01-01 18:00',
+        end: '2075-01-01 22:00',
+        title: 'QC_IRL – Hangout & Chill',
+        description: 'Entspannte Spiele, nette Gespräche und ein paar Drinks. Komm zu unseren qc_irl Treffen jeden 2. und 4. Dienstag im Monat!<br><br>Wir freuen uns auf euch!<br>Euer QC Team ❤️',
+        location: 'Onkel Emma, Echternstraße 9',
+        image: '/images/events/general_chill_event_2025.png',
+        icsFile: '/downloads/qc-irl-events.ics',
+        repeat: {
+            generateAmount: 4,
+            // @ts-ignore
+            generateFunction: () => dayjs().add(-1, 'day').recur().every('Tuesday').daysOfWeek().every([3]).weeksOfMonthByDay(),
         },
     },
     {
